121fc35660b0ac7b26ef9acd79bc92ccc309d1e3
CaptainAlekor <sashakorostelev49@gmail.com>
2022-06-10 04:53:28 +0300

    Added register and login functionality

--- FishingStorePostgress/store/views.py
+++ FishingStorePostgress/store/views.py
@@ -1,14 +1,44 @@
 from datetime import datetime
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 import json
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from .forms import OrderForm, CreateUserForm
 
 from .models import *
 
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    context = {'form':form}
+    return render(request, 'accounts/register.html', context)
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('store')
+
+    context = {}
+    return render(request, 'accounts/login.html', context)
 
 def store(request):
     if request.user.is_authenticated:
-        customer = request.user.customer
+        try:
+            customer = request.user.customer
+        except:
+            customer = Customer.objects.get_or_create(user=request.user, name=request.user.username, email=request.user.email)
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
