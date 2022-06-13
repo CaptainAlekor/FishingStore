c04442e1984f9cd6b92bb6c9f8b6f031deb7ffae
CaptainAlekor <sashakorostelev49@gmail.com>
2022-06-14 00:48:38 +0300

    Fulfilled PEP8 requirements

--- FishingStorePostgress/store/views.py
+++ FishingStorePostgress/store/views.py
@@ -12,6 +12,7 @@ from .models import *
 
 logger = logging.getLogger('main')
 
+
 def registerPage(request):
     logger.info('Now on registration page (views.py registerPage)')
 
@@ -22,8 +23,9 @@ def registerPage(request):
         if form.is_valid():
             form.save()
             return redirect('login')
-    context = {'form':form}
+    context = {'form': form}
     return render(request, 'accounts/register.html', context)
+
 
 def loginPage(request):
     logger.info('Now on login page (views.py loginPage)')
@@ -41,25 +43,37 @@ def loginPage(request):
     context = {}
     return render(request, 'accounts/login.html', context)
 
+
 def logoutUser(request):
     logger.info('User logged out (views.py logoutUser)')
     logout(request)
     return redirect('login')
+
 
 @sync_to_async
 def get_products():
     products = Product.objects.all()
     return products
 
+
 @sync_to_async
 def get_customer(user, name, email):
-    customer = Customer.objects.get_or_create(user=user, name=name, email=email)
+    customer = Customer.objects.get_or_create(
+        user=user,
+        name=name,
+        email=email
+    )
     return customer
+
 
 @sync_to_async
 def get_order(customer, complete):
-    order, created = Order.objects.get_or_create(customer=customer, complete=complete)
+    order, created = Order.objects.get_or_create(
+        customer=customer,
+        complete=complete
+    )
     return order
+
 
 def store(request):
     logger.info('Now on store page (views.py store)')
@@ -68,7 +82,11 @@ def store(request):
         try:
             customer = request.user.customer
         except:
-            customer = asyncio.run(get_customer(request.user, request.user.username, request.user.email))
+            customer = asyncio.run(get_customer(
+                request.user,
+                request.user.username,
+                request.user.email
+            ))
         order = asyncio.run(get_order(customer, False))
         cartItems = order.get_cart_items
     else:
@@ -78,6 +96,7 @@ def store(request):
     products = asyncio.run(get_products())
     context = {'products': products, 'cartItems': cartItems}
     return render(request, 'store/store.html', context)
+
 
 def cart(request):
     logger.info('Now on cart page (views.py cart)')
@@ -95,6 +114,7 @@ def cart(request):
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
 
+
 def checkout(request):
     logger.info('Now on checkout page (views.py checkout)')
     if request.user.is_authenticated:
@@ -110,15 +130,21 @@ def checkout(request):
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/checkout.html', context)
 
+
 @sync_to_async
 def get_product_by_id(prod_id):
     products = Product.objects.get(id=prod_id)
     return products
 
+
 @sync_to_async
 def get_order_item(order, product):
-    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+    orderItem, created = OrderItem.objects.get_or_create(
+        order=order,
+        product=product
+        )
     return orderItem
+
 
 def updateItem(request):
     logger.info('Updating items... (views.py updateItem)')
@@ -141,12 +167,13 @@ def updateItem(request):
         orderItem.quantity = orderItem.quantity - 1
 
     orderItem.save()
-    
+
     if orderItem.quantity <= 0:
         orderItem.delete()
 
     logger.info('Items updated (views.py updateItem)')
     return JsonResponse('Item was added', safe=False)
+
 
 def processOrder(request):
     logger.info('Processing order... (views.py processOrder)')
@@ -155,7 +182,7 @@ def processOrder(request):
 
     if request.user.is_authenticated:
         customer = request.user.customer
-        order = asyncio.run(get_order(customer, False)) 
+        order = asyncio.run(get_order(customer, False))
         total = float(data['form']['total'])
         order.transaction_id = transaction_id
 
@@ -163,7 +190,7 @@ def processOrder(request):
             order.complete = True
         order.save()
 
-        if order.shipping == True:
+        if order.shipping:
             ShippingAddress.objects.create(
                 customer=customer,
                 order=order,
@@ -176,5 +203,4 @@ def processOrder(request):
     else:
         logger.info('User is not logged in... (views.py updateItem)')
 
-    
     return JsonResponse('Payment complete', safe=False)
